fcb640fc1596b4eff17b1f339d57a9c5df789378
ArcaneMage <kevin.henry@javalaboratories.org>
2021-05-02 06:58:51 +0100

    Refined cryptography class

--- src/main/java/org/javalaboratories/healthagents/cryptography/AesCryptography.java
+++ src/main/java/org/javalaboratories/healthagents/cryptography/AesCryptography.java
@@ -1,10 +1,9 @@
 package org.javalaboratories.healthagents.cryptography;
 
 import org.apache.tomcat.util.codec.binary.Base64;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import javax.crypto.*;
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
 import javax.crypto.spec.SecretKeySpec;
 
 import java.io.IOException;
@@ -13,59 +12,58 @@ import java.io.InputStreamReader;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+/**
+ * AES Cryptography utility class to enable encryption and decryption of
+ * string and streams.
+ */
 public final class AesCryptography {
 
-    private final Logger logger = LoggerFactory.getLogger(AesCryptography.class);
-
-    // Can be configured into Constant and read the configuration file for injection
     private static final String KEY = "0246810121416180";
 
-    // The parameters represent the algorithm name / encryption mode / data filling method
-    private static final String ALGORITHM = "AES/ECB/PKCS5Padding";
-    private static final int MAX_BUFFER_SZ = 16;
+    // The algorithm name / encryption mode / data filling method
+    private static final String AES_ALGORITHM = "AES/ECB/PKCS5Padding";
+    private static final int MAX_BUFFER_SZ = 32;
 
     private AesCryptography() {}
 
-
     /**
-     * Encrypts inputStream
+     * Encrypts input stream data
      *
      * @param stream stream to be encrypted
-     * @param encryptKey encryption secret key
-     * @return encrypted resultant string from inputStream.
+     * @param key encryption secret key
+     * @return encrypted resultant string from input stream.
      */
-    public static String encrypt(final InputStream stream, String encryptKey) {
-        String result;
-        try (InputStreamReader reader = new InputStreamReader(stream)) {
-            char[] buffer = new char[MAX_BUFFER_SZ];
-            int read;
-            StringBuilder b = new StringBuilder();
-            while ((read = reader.read(buffer,0,MAX_BUFFER_SZ)) > -1) {
-                b.append(buffer,0,read);
-            }
-            result = b.toString();
-        } catch (IOException e) {
-            throw new IllegalCryptographyStateException("Failed to encrypt input stream",e);
-        }
-        return encrypt(result,encryptKey);
+    public static String encrypt(final InputStream stream, String key) {
+        return encrypt(toString(stream), key);
+    }
+
+    /**
+     * Decrypts input stream data
+     *
+     * @param stream stream to be decrypted
+     * @param key encryption secret key
+     * @return descrypted resultant string from input stream.
+     */
+    public static String decrypt(final InputStream stream, String key) {
+        return decrypt(toString(stream),key);
     }
 
     /**
      * Encryption
      *
-     * @param content encrypted string
-     * @param encryptKey key value
-     * @return
-     * @throws Exception
+     * @param s encrypted string
+     * @param key key value
+     * @return encrypted data.
+     * @throws IllegalCryptographyStateException encryption failure.
      */
-    public static String encrypt(String content, String encryptKey) {
+    public static String encrypt(final String s, final String key) {
         byte[] b;
         try {
             KeyGenerator kgen = KeyGenerator.getInstance("AES");
             kgen.init(128);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(encryptKey.getBytes(), "AES"));
-            b = cipher.doFinal(content.getBytes(UTF_8));
+            Cipher cipher = Cipher.getInstance(AES_ALGORITHM);
+            cipher.init(Cipher.ENCRYPT_MODE, new SecretKeySpec(key.getBytes(UTF_8), "AES"));
+            b = cipher.doFinal(s.getBytes(UTF_8));
         } catch (Exception e) {
             throw new IllegalCryptographyStateException("Failed to encrypt content",e);
         }
@@ -73,21 +71,21 @@ public final class AesCryptography {
     }
 
     /**
-     * Decrypt
+     * Decryption
      *
-     * @param encryptStr decrypted string
-     * @param decryptKey decrypted key value
-     * @return
-     * @throws Exception
+     * @param s decrypted string
+     * @param key decrypted key value
+     * @return decrypted data.
+     * @throws IllegalCryptographyStateException decryption failure.
      */
-    public static String decrypt(String encryptStr, String decryptKey)  {
+    public static String decrypt(final String s, final String key)  {
         byte[] decryptBytes;
         try {
             KeyGenerator kgen = KeyGenerator.getInstance("AES");
             kgen.init(128);
-            Cipher cipher = Cipher.getInstance(ALGORITHM);
-            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(decryptKey.getBytes(), "AES"));
-            byte[] encryptBytes = Base64.decodeBase64(encryptStr);
+            Cipher cipher = Cipher.getInstance(AES_ALGORITHM);
+            cipher.init(Cipher.DECRYPT_MODE, new SecretKeySpec(key.getBytes(UTF_8), "AES"));
+            byte[] encryptBytes = Base64.decodeBase64(s.getBytes(UTF_8));
             decryptBytes = cipher.doFinal(encryptBytes);
         } catch (Exception e) {
             throw new IllegalCryptographyStateException("Failed to decrypt content",e);
@@ -95,15 +93,35 @@ public final class AesCryptography {
         return new String(decryptBytes);
     }
 
-    public static String encrypt(InputStream content) {
+    public static String encrypt(final InputStream content) {
         return encrypt(content, KEY);
     }
 
-    public static String encrypt(String content) {
+    public static String decrypt(final InputStream content) {
+        return decrypt(content, KEY);
+    }
+
+    public static String encrypt(final String content) {
         return encrypt(content, KEY);
     }
 
-    public static String decrypt(String encryptStr) {
+    public static String decrypt(final String encryptStr) {
         return decrypt(encryptStr, KEY);
+    }
+
+    private static String toString(final InputStream stream) {
+        String result;
+        try (InputStreamReader reader = new InputStreamReader(stream)) {
+            char[] buffer = new char[MAX_BUFFER_SZ];
+            int read;
+            StringBuilder b = new StringBuilder();
+            while ((read = reader.read(buffer, 0, MAX_BUFFER_SZ)) > -1) {
+                b.append(buffer, 0, read);
+            }
+            result = b.toString();
+        } catch (IOException e) {
+            throw new IllegalCryptographyStateException("Failed to process input stream",e);
+        }
+        return result;
     }
 }
--- src/test/java/org/javalaboratories/healthagents/cryptography/AesCryptographyTest.java
+++ src/test/java/org/javalaboratories/healthagents/cryptography/AesCryptographyTest.java
@@ -2,6 +2,9 @@ package org.javalaboratories.healthagents.cryptography;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AesCryptographyTest {
@@ -9,7 +12,7 @@ public class AesCryptographyTest {
     private AesCryptography cryptography;
 
     @Test
-    public void testAesCryptography_Encrypt_Pass() {
+    public void testAesCryptography_EncryptString_Pass() {
         String s = "This is a test of encryption, but should be ok";
 
         String result = AesCryptography.encrypt(s);
@@ -17,8 +20,24 @@ public class AesCryptographyTest {
     }
 
     @Test
-    public void testAesCryptography_Decrypt_Pass() {
+    public void testAesCryptography_EncryptStream_Pass() {
+        InputStream s = new ByteArrayInputStream("This is a test of encryption, but should be ok".getBytes());
+
+        String result = AesCryptography.encrypt(s);
+        assertEquals("2zlIfm+Xo4miqwA4Azkl9Q5sXq1xIiP7zHwd4KrKDkn4jF4ebERaMS0DOR4BlYs6",result);
+    }
+
+    @Test
+    public void testAesCryptography_DecryptString_Pass() {
         String s = "2zlIfm+Xo4miqwA4Azkl9Q5sXq1xIiP7zHwd4KrKDkn4jF4ebERaMS0DOR4BlYs6";
+
+        String result = AesCryptography.decrypt(s);
+        assertEquals("This is a test of encryption, but should be ok",result);
+    }
+
+    @Test
+    public void testAesCryptography_DecryptStream_Pass() {
+        InputStream s = new ByteArrayInputStream("2zlIfm+Xo4miqwA4Azkl9Q5sXq1xIiP7zHwd4KrKDkn4jF4ebERaMS0DOR4BlYs6".getBytes());
 
         String result = AesCryptography.decrypt(s);
         assertEquals("This is a test of encryption, but should be ok",result);
